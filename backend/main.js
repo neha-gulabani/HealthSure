ca4fd4617842b895428e9afd4d5e049b07f84d5a
suvedhav <109465078+suvaderv@users.noreply.github.com>
2022-08-04 22:41:15 +0800

    mood tracking backend

--- backend/main.js
+++ backend/main.js
@@ -1,73 +1,82 @@
 module.exports = function (app) {
-  // display main page
-  app.get("/mainpg", function (req, res) {
-    res.render("mainpg.html");
-  });
+    
+    // display main page
+    // app.get("/mainpg", function (req, res) {
+    //     res.render("mainpg.html");
+    // });
 
-  // display sign-up page
-  app.get("/signup", function (req, res) {
-    res.render("signup.html");
-  });
+    // display sign-up page
+    // app.get("/signup", function (req, res) {
+    //     res.render("signup.html");
+    // });
 
-  // display login page
-  app.get("/login", function (req, res) {
-    res.render("login.html");
-  });
+    // display login page
+    // app.get("/login", function (req, res) {
+    //     res.render("login.html");
+    // });
 
-  // sign-up page: collect form data and insert into table 'users'
-  app.post("/signed", function (req, res) {
-    let newrecord = [
-      req.body.firstname,
-      req.body.lastname,
-      req.body.username,
-      req.body.email,
-      req.body.contactno,
-    ];
-    // let hashedpw = await bcrypt.hash(req.body.password, 10);
+    // sign-up page: collect form data and insert into table 'users'
+    app.post("/signed", function (req, res) {
+        let newrecordE = [req.body.email];
+        let newrecord = [req.body.firstname, req.body.lastname,
+                        req.body.username, req.body.password,
+                        req.body.email, req.body.contactno];
 
-    let sqlquery = "SELECT * FROM 'users' WHERE username like ?";
-    let sqlquery1 = "INSERT INTO users VALUES ('?','?','?','?','?','?')";
+        let sqlquery = "SELECT * FROM users WHERE username = ?";
+        let sqlquery1 = "INSERT INTO users VALUES (0,?,?,?,?,?,?)";
 
-    db.query(sqlquery, newrecord, (err, result) => {
-      if (err) throw err;
-      if (result.length != 0) {
-        res.send("User already exists");
-        res.redirect("/login");
-      } else {
-        db.query(sqlquery1, newrecord, (err, result) => {
-          if (err) {
-            return console.error(err.message);
-          } else {
-            res.send("Created new user");
-            res.render("homepage.html", { users: result });
-          }
+        db.query(sqlquery, newrecordE, (err, result) => {
+            if (err) throw err;
+            console.log(result.length);
+            if (result.length != 0) {
+                // res.send("User already exists");
+                res.redirect("/mainPage");
+            }
+            else {
+                db.query(sqlquery1, newrecord, (err, result) => {
+                    if (err) {
+                        return console.error(err.message);
+                    } else {
+                        // res.send("Created new user");
+                        res.render("Home", { users: result });
+                    }
+                });
+            }
         });
-      }
     });
-  });
 
-  // login page: collect form data and insert into table 'users'
-  app.post("/logined", function (req, res) {
-    let uname = [req.body.username];
-    let passw = [req.body.password];
+    // login page: collect form data and insert into table 'users'
+    app.post("/logined", function (req, res) {
+        let newrecord = [req.body.username];
 
-    let sqlquery = "SELECT * FROM 'users' WHERE username like ?";
-
-    db.query(sqlquery, uname, passw, (err, result) => {
-      if (err) {
-        res.send("No such user logged in");
-        res.redirect("/login");
-      }
-      // else {
-      //     let hashedpw = result[0].passw;
-      //     if (await bcrypt.compare(passw, hashedpw)) {
-      //         res.render("homepage.html", { users: result });
-      //     }
-      //     else {
-      //         res.send("Invalid password");
-      //         res.redirect("/login");
-      //     }
-      // }
+        let sqlquery = "SELECT * FROM users WHERE username = ?";
+        
+        db.query(sqlquery, newrecord, (err, result) => {
+            if (err) throw err;
+            console.log(result.length);
+            if (result.length == 0) {
+                // res.send("User does not exist");
+                res.redirect("/login");
+            } 
+            else {
+                // res.send("Logged in");
+                res.render("Home", { users: result });
+            }
+        });
     });
-  });
-};
+
+    // mood quiz page: collect form data and insert into table 'mood_emos'
+    app.post("/mood_chosen", function (req, res) {
+        let newrecord = [req.body.mood, req.body.planet];
+
+        let sqlquery = "INSERT INTO mood_emos VALUES (0,?,?)";
+
+        db.query(sqlquery, newrecord, (err, result) => {
+            if (err) {
+                res.redirect("/MoodQuiz");
+            }
+            // res.send(result);
+            res.render("MoodReport", { mood_emos: result });
+        });
+    })
+}

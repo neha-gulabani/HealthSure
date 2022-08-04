acd4409fe70a660c95b602063bd699ba2a2e866f
suvedhav <109465078+suvaderv@users.noreply.github.com>
2022-08-04 22:39:16 +0800

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,26 +1,30 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD6GyiMdunSzJOHTeTnbauHSw5hSXy0duc",
-  authDomain: "healthsure-9dd18.firebaseapp.com",
-  projectId: "healthsure-9dd18",
-  storageBucket: "healthsure-9dd18.appspot.com",
-  messagingSenderId: "951524258216",
-  appId: "1:951524258216:web:0728ec3d7e9d34f9d26d9d",
+  apiKey: "AIzaSyA6MPOdXlpQh22p944NZ19jmqz-mtHQBz8",
+  authDomain: "healthsure-2115a.firebaseapp.com",
+  projectId: "healthsure-2115a",
+  storageBucket: "healthsure-2115a.appspot.com",
+  messagingSenderId: "687088350747",
+  appId: "1:687088350747:web:fe4baab7dbfff060e0aabe"
 };
 
 // Initialize Firebase
 let app;
-if (firebase.apps.length == 0) {
-  app = firebase.initializeApp(firebaseConfig);
+if (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig);
 } else {
-  app = firebase.app();
+    app = firebase.app()
 }
 
-const auth = firebase.auth();
+const auth = firebase.auth()
 
 export { auth };
